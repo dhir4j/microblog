ea85dea14a8c5db5ba23bdb7fdb7c714c9a63691
dhir4j <kapsedhiraj15@gmail.com>
2024-03-26 01:12:45 +0530

    pagination support

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,48 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.models import User
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
+from app.models import User, Post
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
 
-@app.route('/')
-@app.route('/index')
+
+
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'dhir4j' }
-    
-    posts = [
-        {   
-            'author': {'username':'John'},
-            'body':'Beautiful day in Poland!'
-        },
-        {   
-            'author': {'username':'Mac'},
-            'body':'The GodFather movie was cool!'
-        }
-    ]
-    return render_template('index.html', title="Home  Page", posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts = db.paginate(current_user.following_posts(), page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/explore')
+def explore():
+    page = request.args.get('page', 1, type=int)
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET','POST'])    
 def login():
@@ -66,14 +85,19 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
-    user = db.first_or_404(sa.Select(User).where(User.username == username))
-    posts = [
-        {'author':user, 'body':'Test post #1'},
-        {'author':user, 'body':'Test post #2'}
-    ]
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    page = request.args.get('page', 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'],
+                        error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
-
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=form)
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
@@ -136,3 +160,5 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+    
+
